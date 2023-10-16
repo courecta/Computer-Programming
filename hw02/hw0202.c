74a8357a3f94a549982c22e612cebe6fce5077ce
courecta <41247044Sntnu.edu.tw>
2023-10-16 22:17:11 +0800

    ...

--- hw02/hw0202.c
+++ hw02/hw0202.c
@@ -4,7 +4,9 @@
 
 int main(){
 
-  int64_t number1, number2, product, limit = pow(2, 31);
+  // Input checking
+  int64_t number1 = 0, number2 = 0, product = 0, limit = pow(2, 31);
+  int16_t zero1 = 0, zero2 = 0, zeroProd = 0;
     printf("PLease enter the first number: ");
     scanf("%ld", &number1);
     printf("PLease enter the second number: ");
@@ -15,68 +17,156 @@ int main(){
       return 1;
     }
 
+    // Space formatting
     product = number1 * number2;
+    int64_t divisorProd = 1000000000;
+    int32_t lineCount = 0, digitCount, zeroMet = 0;
+    while (divisorProd > 0){
+      digitCount = (product / divisorProd) % 10;
+      if (digitCount == 0 && zeroMet == 0){
+      }
+      else if (digitCount != 0 && zeroMet == 0){
+        zeroMet += 1;
+      }
+      if (zeroMet != 0){
+        lineCount += 1;
+      }
+      divisorProd /= 10;
+    }
+
+    int32_t spaceDiv1 = 1000000000, zeroMet1 = 0, firstCount = 0, spaceCount1;
+    while (spaceDiv1 > 0) {
+      spaceCount1 = (number1 / spaceDiv1) % 10;
+      if (spaceCount1 == 0 && zeroMet1 == 0){
+      }
+      else if (spaceCount1 != 0 && zeroMet1 == 0){
+        zeroMet1 += 1;
+      }
+      if (zeroMet1 != 0){
+        firstCount += 1;
+      }
+      spaceDiv1 /= 10;
+    }
+
+    int32_t spaceDiv2 = 1000000000, zeroMet2 = 0, secondCount = 0, spaceCount2;
+    while (spaceDiv2 > 0) {
+      spaceCount2 = (number2 / spaceDiv2) % 10;
+      if (spaceCount2 == 0 && zeroMet2 == 0){
+      }
+      else if (spaceCount2 != 0 && zeroMet2 == 0){
+        zeroMet2 += 1;
+      }
+      if (zeroMet2 != 0){
+        secondCount += 1;
+      }
+      spaceDiv2 /= 10;
+    }
+
+    int16_t lines = (2 * lineCount) + 1, lines1 = (2 * firstCount) + 1, lines2 = (2 * secondCount) + 1;
+
+    int16_t longestLine = 0;
+    if (number1 >= number2 && number1 >= product){
+      longestLine += lines1;
+    }
+    else if (number2 >= number1 && number2 >= product){
+      longestLine += lines2;
+    }
+    else{
+      longestLine += lines;
+    }
+
+    int16_t simple1 = 0, simple2 = 0;
+    if (number1 == 0){
+      simple1 += 1;
+    }
+    if (number2 == 0){
+      simple2 += 1;
+    }
+
+    int16_t space1 = longestLine - (2 * firstCount);
+    for (int16_t i = 0 ; i <= space1 ; i++){
+      printf(" ");
+    }
+
     int64_t divisor1 = 1000000000, divisor2 = 1000000000;
+    if (simple1 == 1){
+      printf("\b\b0");
+    }
+    else if (simple1 != 1){
     while (divisor1 > 0){
       int32_t digitPlace1 = (number1 / divisor1) % 10;
-      if (digitPlace1 == 0 && int32_t zero1 == 0){
-        printf(" ");
+      if (digitPlace1 == 0 && zero1 == 0){
       }
       else {
-      printf("%d", digitPlace1);
+      printf("%d ", digitPlace1);
       zero1 += 1;
       }
-      if (divisor1 > 1){
-        printf(" ");
-      }
       divisor1 /= 10;
+    }
     }
     printf("\n");
     printf("*)");
 
+    int16_t space2 = longestLine - (2 * secondCount);
+    for (int16_t i = 0 ; i <= space2 - 2 ; i++){
+      printf(" ");
+    }
+
+    if (simple2 == 1){
+    printf("\b\b0");
+    }
+    else if (simple2 != 1){
     while (divisor2 > 0){
       int32_t digitPlace2 = (number2 / divisor2) % 10;
-      if (digitPlace2 == 0 && int32_t zero2 == 0){
-        printf(" ");
+      if (digitPlace2 == 0 && zero2 == 0){
       }
       else {
       printf("%d ", digitPlace2);
       zero2 += 1;
       }
-
-      if (divisor2 > 1){
-        printf(" ");
-      }
       divisor2 /= 10;
     }
+  }
     printf("\n");
+    divisorProd = 1000000000;
 
-    while ()
-    
-    for (int32_t i = 0 ; i >= (2 * product) + 1 ; i++){
+    for (int32_t i = 0 ; i < longestLine ; i++){
       printf("-");
     }
+    printf("\n");
+
+    // Partial product calculations and formatting
+
     
     printf("\n");
 
+    // Outputting and formatting the product
+    if (product == 0 || number2 <= 9){
+    for (int32_t i = 0 ; i < longestLine ; i++){
+      printf("-");
+    }
+    printf("\n");
+    }
+    if (product == 0){
+      for (int16_t i = 0 ; i < longestLine - 1 ; i++){
+        printf(" ");
+      }
+      printf("0\n");
+    }
+    else {
     printf("  ");
-    int32_t divisorProd = 1000000000;
     while (divisorProd > 0){
       int32_t digitPlaceProd = (product / divisorProd) % 10;
-      if (digitPlaceProd == 0 && int32_t zeroProd == 0){
-        printf(" ");
+      if (digitPlaceProd == 0 && zeroProd == 0){
       }
       else {
       printf("%d ", digitPlaceProd);
       zeroProd += 1;
       }
-
-      if (divisorProd > 1){
-        printf(" ");
-      }
       divisorProd /= 10;
     }
     printf("\n");
+  }
 
   return 0;
 }
--- hw02/hw0203.c
+++ hw02/hw0203.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
 
+  int32_t number;
+
 int main () {
 
-    
+    for (;;){
+      if ( number != 0){
+        printf("Please enter an Integer: ");
+        scanf("%d", &number);
+      }
+    }
 
   return 0;
 }
