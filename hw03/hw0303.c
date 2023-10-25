56c758f0b21497fefcb5241b687f9351a3c0fc2f
courecta <41247044Sntnu.edu.tw>
2023-10-25 15:39:43 +0800

    ...

--- hw03/hw0303.c
+++ hw03/hw0303.c
@@ -1,9 +1,65 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
+
+int32_t number;
+
+int32_t BinaryForm (int32_t number);
+int32_t twosCompliment (int32_t number);
+int32_t Counter (int32_t count);
 
 int main(){
 
-    
+    printf("Please enter the number: ");
+    scanf("%d", &number);
+    printf("binary = %d, two's compliment = %d, and count is %d\n", BinaryForm(number), Counter(count));
+
+    if (number > 0){
+        for (int32_t i = 0 ; i < Counter(count) ; i++){
+            if (i % 8 == 0){
+                printf(" ");
+                continue;
+            }
+            printf("0");
+        }
+    }
+    else if (number < 0){
+        for (int32_t i = 0 ; i < Counter(count) ; i++){
+            if (i % 8 == 0){
+                printf(" ");
+                continue;
+            }
+            printf("1");
+        }
+    }
 
     return 0;
+}
+
+int32_t BinaryForm (int32_t number){
+    int32_t binaryNumber = 0;
+    for (int32_t i = 0; number != 0; i++){
+        int32_t remainderCount = number % 2;
+        int32_t powerTo32 = pow(10, i);
+        
+        binaryNumber = binaryNumber + (remainderCount * powerTo32);
+        number /= 2;
+    }
+    return binaryNumber;
+
+}
+
+int32_t twosCompliment (int32_t number){
+    BinaryForm(binaryNumber);
+
+}
+
+int32_t Counter (int32_t count){
+    if (number > 0){
+    BinaryForm (binaryNumber);
+    }
+    else if (number < 0){
+        
+    }
+
 }
