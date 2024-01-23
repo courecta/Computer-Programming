38bc7df639734000e3407b69f5b20386fa4cb658
Courecta <90601835+Courecta@users.noreply.github.com>
2024-01-23 18:09:34 +0000

    ...

--- Test/test.c
+++ Test/test.c
@@ -2,5 +2,17 @@
 
 int main(){
     
+    uint16_t n = 0;
+    //double sqrt2 = sqrt(2);
+    printf("Please enter n (16-bit unsigned): ");
+    scanf("%hu", &n);
+
+    double val = 1.0;
+    for(uint16_t i = 1 ; i <= n ; i++)
+    {
+        val = 1.0 + (1.0 / (1.0 + val));    
+        printf("n = %hu: %.15lf (%.15lf) ", i, val, val - sqrt(2));
+    }
+
     return 0;
 }
