aa8c6a7784b9fba205677c7c8e9d2634658b08ad
courecta <41247044S@ntnu.edu.tw>
2024-02-24 01:02:35 +0800

    changed stuff and fixed typo

--- programming/2023/rework/hw02/hw0202.c
+++ programming/2023/rework/hw02/hw0202.c
@@ -2,5 +2,16 @@
 
 int main(){
 
+  // a represents the first number while b represents the second number
+  uint32_t a = 0, b = 0;
+
+
+  printf("Please enter the first number");
+  scanf("%d", &a);
+  printf("Please enter the second number");
+  scanf("%d", &b);
+
+  multiply(a, b);
+
     return 0;
 }
