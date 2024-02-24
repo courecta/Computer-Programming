9b4fb00a932f762e15a7773e6db322040908fb27
courecta <41247044S@ntnu.edu.tw>
2024-02-24 14:12:15 +0800

    worked on hw0202 files

--- programming/2023/rework/hw02/myMultiplication.c
+++ programming/2023/rework/hw02/myMultiplication.c
@@ -1,2 +1,30 @@
 #include "myMultiplication.h"
 
+void multiply(const uint32_t firstNum, const uint32_t secondNum)
+{
+  print_num(firstNum);
+  print_num(secondNum);
+
+// TODO setup all data to be int64_t and test for boundaries
+
+  return;
+}
+
+void print_num(const uint32_t num)
+{
+  uint32_t temp = num;
+  while(temp != )
+
+  return;
+}
+
+void print_line(const uint32_t num)
+{
+  printf("\n");
+  for(int32_t i = 0; i < (2 * num) + 1) ; i++) printf("-");
+  printf("\n");
+  return;
+}
+
+
+
--- programming/2023/rework/hw02/myMultiplication.h
+++ programming/2023/rework/hw02/myMultiplication.h
@@ -9,3 +9,7 @@
 #include <stdbool.h>
 #include <math.h>
 
+// function prototypes
+void multiply(const uint32_t firstNum, const uint32_t secondNum);
+void print_num(const uint32_t num);
+void print_line(const uint32_t num);
