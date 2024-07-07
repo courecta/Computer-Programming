9cdacbf0366ad4bd11dbb463dffdf9215ec04bf3
courecta <41247044S@ntnu.edu.tw>
2024-07-08 00:42:36 +0800

    completed 0204

--- programming/2023/rework/hw02/hw0204.c
+++ programming/2023/rework/hw02/hw0204.c
@@ -1,9 +1,83 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 
 int main()
 {
 
-  
+  uint64_t initial = 0;
+  int64_t recurring = 0;
+  uint8_t startMonth = 0;
+  uint16_t startYear = 0;
+  uint8_t endMonth = 0;
+  uint16_t endYear = 0;
+  double rate = 0;
+  uint64_t total = 0;
+  double netIncome = 0.0, currentRate = 0.0;
+
+  printf("Initial Investment: ");
+  scanf("%lu", &initial);
+
+  printf("Recurring Monthly Investment: ");
+  scanf("%ld", &recurring);
+
+  printf("Start Month: ");
+  scanf("%hhu", &startMonth);
+
+  printf("Start Year: ");
+  scanf("%hu", &startYear);
+
+  printf("End Month: ");
+  scanf("%hhu", &endMonth);
+
+  printf("End Year: ");
+  scanf("%hu", &endYear);
+
+  printf("Annual Rate of Return (%%): ");
+  scanf("%lf", &rate);
+
+  rate /= 1200;
+
+  printf("--- Output ---\n");
+
+  // handle error
+
+  if(initial < 1 || initial > pow(10, 7) || recurring < 0 || recurring > pow(10, 7) || startMonth < 1 || startMonth > 12 || endMonth < 1 || endMonth > 12 || startYear < 1 || endYear < 1 || startYear > pow(10, 4) || endYear > pow(10, 4) || rate < 1 || rate > 100)
+  {
+    printf("error!\n");
+    return 0;
+  }
+
+  uint16_t i = startYear, j = startMonth;
+
+  while(i < endYear || j <= endMonth)
+  {
+    printf("%u.%u) ", i, j);
+
+    printf("%lu/", initial);
+
+    total = initial + netIncome;
+
+    printf("%lu/", total);
+
+    printf("%.0lf/", floor(netIncome));
+
+    netIncome = netIncome + (total * rate);
+
+    currentRate = (netIncome / total) * 100;
+
+    printf("%.2lf%%", currentRate - 1.00);
+
+    printf("\n");
+
+    initial += recurring;
+
+    if((j + 1) > 12)
+    {
+      j = 1;
+      i++;
+    } else j++;
+  }
+
   return 0;
 }
--- programming/2023/rework/hw02/hw0205.c
+++ programming/2023/rework/hw02/hw0205.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdint.h>
 
+void catan(int64_t length, int64_t number);
+
 int main()
 {
+  int64_t length = 0, number_layer = 0;
+  printf("Please input the length: ");
+  scanf("%ld", &length);
+  printf("Please input the number of layer: ");
+  scanf("%ld", &number_layer);
 
+  catan(length, number_layer);
   
   return 0;
+}
+
+void catan(int64_t length, int64_t number){
+
+
+
+  return;
 }
